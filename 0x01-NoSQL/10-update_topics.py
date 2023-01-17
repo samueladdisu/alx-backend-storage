5a93ac67fb33fac7dc56fedccbe864abd5a68642
samueladdisu <samueladdisu7@gmail.com>
2023-01-17 08:34:13 -0800

    topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" update doc """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics """
+    update = mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+    return update
